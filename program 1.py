d62f2390540eaea484c165db96497b7919b281af
trblair <trblair@students.unwsp.edu>
2024-11-15 22:00:32 -0600

    Update program 1.py

--- program 1.py
+++ program 1.py
@@ -2,7 +2,7 @@ import tkinter
 window=tkinter.Tk()
 # add widgets here
 lbl=tkinter.Label(window, text="Life isnt about waiting for storms to pass, it's about learning how to dance in the rain ", font=("Helvetica", 16))
-lbl.place(x=60, y=50)
+lbl.pack()
 
 window.title('My GUI')
 window.geometry("1000x200+10+20")
