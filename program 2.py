42a1ecb2e0e0a6750a63b8ddafea5573194b4ac1
trblair <trblair@students.unwsp.edu>
2024-11-15 22:16:38 -0600

    Update program 2.py

--- program 2.py
+++ program 2.py
@@ -1,9 +1,14 @@
 import tkinter
 import tkinter.messagebox
-win= tkinter.Tk()
-win.geometry("200x150")
+window= tkinter.Tk()
+window.geometry("200x150")
 def show_msg():
-#I would prefer to not post my adress in plaintext, but the program should work
+#I would prefer to not post my address, but the program should work
    tkinter.messagebox.showinfo("Info", "My name is Tristan Blair,\n I live at [Address]")
-tkinter.Button(win, text= "Show Info", command=show_msg).pack()
-win.mainloop()
+def quit():
+   window.destroy()
+
+tkinter.Button(window, text= "Show Info", command=show_msg).pack()
+tkinter.Button(window, text= "Quit", command=quit).pack()
+
+window.mainloop()
